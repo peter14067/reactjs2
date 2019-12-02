c8526001f6662a841aa4487d08bdafdc7dc8032d
peter14067 <peter14067@gmail.com>
2019-12-02 17:39:56 +0800

    Updates

---
+++ precache-manifest.fad4b467320f2af4f1f34769f731bce2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a6d607b9ee2d970e1ab86a338c9154",
+    "url": "/reactjs1/index.html"
+  },
+  {
+    "revision": "1b6953bc51205fcabaa9",
+    "url": "/reactjs1/static/css/2.a9858c30.chunk.css"
+  },
+  {
+    "revision": "ade24f0f572d9360a31f",
+    "url": "/reactjs1/static/css/main.afbe53de.chunk.css"
+  },
+  {
+    "revision": "1b6953bc51205fcabaa9",
+    "url": "/reactjs1/static/js/2.03d150ae.chunk.js"
+  },
+  {
+    "revision": "ade24f0f572d9360a31f",
+    "url": "/reactjs1/static/js/main.dc6a53c0.chunk.js"
+  },
+  {
+    "revision": "e8757b5806da09748d2f",
+    "url": "/reactjs1/static/js/runtime-main.b6229466.js"
+  },
+  {
+    "revision": "dd861778d5e8672b2d246ca543399608",
+    "url": "/reactjs1/static/media/pc11.dd861778.jpg"
+  },
+  {
+    "revision": "1f5d80be7a87827088b9f6c5f85f21cd",
+    "url": "/reactjs1/static/media/pc3.1f5d80be.jpg"
+  }
+]);
